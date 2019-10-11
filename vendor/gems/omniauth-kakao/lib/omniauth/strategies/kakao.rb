362f80c8af556c8b0ab399a9e9be100b25360bcc
rest515 <rest515@gmail.com>
2019-10-11 20:46:43 +0900

    add default gems #3

---
+++ vendor/gems/omniauth-kakao/lib/omniauth/strategies/kakao.rb
@@ -0,0 +1,58 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class Kakao < OmniAuth::Strategies::OAuth2
+      DEFAULT_REDIRECT_PATH = "/oauth"
+
+      option :name, 'kakao'
+
+      option :client_options, {
+        :site => 'https://kauth.kakao.com',
+        :authorize_path => '/oauth/authorize',
+        :token_url => '/oauth/token',
+      }
+
+      uid { raw_info['id'].to_s }
+
+      info do
+        {
+          'name' => raw_properties['nickname'],
+          'image' => raw_properties['thumbnail_image'],
+        }
+      end
+
+      extra do
+        {'properties' => raw_properties}
+      end
+
+      def initialize(app, *args, &block)
+        super
+        options[:callback_path] = options[:redirect_path] || DEFAULT_REDIRECT_PATH
+      end
+
+      def callback_phase
+        previous_callback_path = options.delete(:callback_path)
+        @env["PATH_INFO"] = "/users/auth/kakao/callback"
+        options[:callback_path] = previous_callback_path
+        super
+      end
+
+      def mock_call!(*)
+        options.delete(:callback_path)
+        super
+      end
+
+    private
+      def raw_info
+        @raw_info ||= access_token.get('https://kapi.kakao.com/v1/user/me', {}).parsed || {}
+      end
+
+      def raw_properties
+        @raw_properties ||= raw_info['properties']
+      end
+    end
+  end
+end
+
+OmniAuth.config.add_camelization 'kakao', 'Kakao'
