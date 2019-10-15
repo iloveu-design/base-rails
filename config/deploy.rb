bad0f1eccb8d2ad3df57e7a152c809b2a77947fb
Ohyeon <rest515@gmail.com>
2019-10-15 12:10:10 +0900

    set deployment environment for staging

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+# config valid only for current version of Capistrano
+lock "3.11.2"
+
+set :application, 'base-rails'
+set :repo_url, 'git@github.com:slowalk/base-rails.git'
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :ssh_options, {forward_agent: true}
+
+# set :bundle_binstubs, nil
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads public/files}
+
+set :keep_releases, 5
+
+namespace :unicorn do
+  Rake::Task["restart"].clear_actions
+  desc "restart unicorn"
+  task :restart do
+    on roles :app do
+      sudo "/etc/init.d/#{fetch(:unicorn_service)}", 'restart'
+    end
+  end
+end
---
+++ config/deploy/staging.rb
@@ -0,0 +1,25 @@
+set :stage, :staging
+set :branch, "master"
+set :rails_env, "staging"
+set :nginx_server_name, 'base-rails.slowalk.dev'
+set :deploy_user, 'ufofactory'
+set :deploy_to, '/home/ufofactory/apps/base-rails'
+
+set :rbenv_ruby, '2.5.1'
+
+role :app, %w{ufofactory@slowalk.dev}
+role :web, %w{ufofactory@slowalk.dev}
+role :db,  %w{ufofactory@slowalk.dev}
+server 'slowalk.dev', user: 'ufofactory', roles: %w{web app}, my_property: :my_value
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+end
