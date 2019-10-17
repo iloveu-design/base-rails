142ec80e55297b8919bc8e8177fa74276d5aec5a
Ohyeon <rest515@gmail.com>
2019-10-17 15:32:19 +0900

    set default layout for application and admin #8

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
 
+  resources :subscriptions
+
   namespace :admin do
-    root 'users#index'
+    root 'subscriptions#index'
     resources :subscriptions
   end
 end
