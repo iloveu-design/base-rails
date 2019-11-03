21dcd6717a72fe461ee983cc2f1fd31e3ed7e894
Ohyeon <rest515@gmail.com>
2019-11-03 12:46:28 +0900

    Refactor devise related files.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,30 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
   def facebook
     data = request.env["omniauth.auth"]
     @user = User.find_sns_oauth(data["uid"], 'facebook')
@@ -63,5 +89,3 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 end
-
-
