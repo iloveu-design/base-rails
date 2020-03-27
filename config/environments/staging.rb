38f4265d4704311b3ef54b0b39c33f66d3abaae5
Soyu Kim <mmx900@gmail.com>
2020-03-27 13:27:05 +0900

    실제 사이트에 사용되는 도메인이나 고유명사들을 삭제합니다 #60

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "start-rails_#{Rails.env}"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = { :host => 'sygc.ufosoft.net' }
-  config.action_mailer.asset_host =  'http://sygc.ufosoft.net'
+  config.action_mailer.default_url_options = { :host => 'rails.ufosoft.net' }
+  config.action_mailer.asset_host =  'http://rails.ufosoft.net'
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
